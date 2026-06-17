dbcac42826d6018e4b72ed64b4d66fb3ed5e8c0c
svetlana riasnova <r_s_u@outlook.com>
2026-06-17 14:48:18 +0300

    Add backend: tasks endpoints

--- backend/server.js
+++ backend/server.js
@@ -16,22 +16,7 @@ app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
-app.get('/tasks', (req, res) => {
-    const query = `SELECT * FROM  tasks ORDER BY id DESC`;
 
-    db.all(query, (err, rows) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json(rows);
-    });
-});
-
-app.get('/tasks/:id', (req, res) => {
-
-})
-// Ручка для создания задачи (POST /tasks)
-// Сюда мы будем отправлять данные из Vue, чтобы сохранить задачу в БД
 app.post('/tasks', (req, res) => {
   console.log('📦 Тело запроса:', req.body);
   const { title, description } = req.body;
@@ -43,9 +28,7 @@ app.post('/tasks', (req, res) => {
   const query = `INSERT INTO tasks (title, description) VALUES (?, ?)`;
 
   db.run(query, [title, description || ''], function(err) {
-
     if (err) {
-
       return res.status(500).json({ error: err.message });
     }
 
@@ -58,6 +41,99 @@ app.post('/tasks', (req, res) => {
     });
   });
 });
+
+app.get('/tasks', (req, res) => {
+    const query = `SELECT * FROM  tasks ORDER BY id DESC`;
+
+    db.all(query, (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT * FROM tasks WHERE id = ?`;
+
+  db.get(query, [id], (err, row) => {
+    if (err) {
+      return res.starus(500).json({ error: err.message})
+    } 
+    if (!row) {
+      return res.status(404).json({ error: 'Задача не найдена'});
+    }
+    res.json(row);
+  });
+});
+
+app.put('/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  const{ title, description, completed} = req.body;
+
+  if (title === undefined && description === undefined && completed === undefined) {
+    return res.status(400).json({ error: 'Не переданы поля для обновления'})
+  }
+
+  let query = 'UPDATE tasks SET ';
+  const params = [];
+  const updates = [];
+
+  if (title !== undefined) {
+    updates.push('title = ?');
+    params.push(title);
+  }
+
+  if (description !== undefined) {
+    updates.push('description = ?');
+    params.push(description);
+  }
+
+  if (completed !== undefined) {
+    updates.push('completed = ?');
+    params.push(completed);
+  }
+
+  query += updates.join(', ');
+  query += ' WHERE id = ?';
+  params.push(id);
+
+  db.run(query, params, function(err) {
+    if (err) {
+      return res.status(500).json({ error: err.message}); 
+    }
+    if (this.changes === 0) {
+      return res.status(404).json({ error: 'Задача не найдена'})
+    }
+
+    res.json({
+      id: parseInt(id),
+      title: title !== undefined ? title : undefined,
+      description: description !== undefined ? description : undefined,
+      completed: completed !== undefined ? completed : undefined,
+      message: '✅ Задача обновлена'
+    })
+  })
+})
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  const query =  `DELETE FROM tasks WHERE id = ?`;
+
+  db.run( query, [id], function(err) {
+    if (err) {
+      return res.status(500).json({ error: err.message});
+    }
+
+    if (this.changes === 0) {
+      return res.status(404).json({ error: 'Задача не найдена'});
+    }
+
+    res.json({ message: '🗑️ Задача удалена'});
+  })
+
+})
 
 app.listen(PORT, () => {
     console.log(`✅ Сервер запущен на http://localhost:${PORT}`);
